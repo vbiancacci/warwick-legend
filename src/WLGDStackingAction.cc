7a26a607d61024fe9b228dfb1c01c403ab242abc
Moritz Neuberger <moritz.neuberger@tum.de>
2021-03-04 10:05:58 +0100

    Adjusted LAr def. to use prev. verion in new mix.

--- src/WLGDStackingAction.cc
+++ src/WLGDStackingAction.cc
@@ -3,6 +3,14 @@
 
 G4ClassificationOfNewTrack WLGDStackingAction ::ClassifyNewTrack(const G4Track* aTrack)
 {
+
+  if(aTrack->GetParticleDefinition()->GetParticleName() == "neutron")
+  {
+    G4cout << "Pre: " << aTrack->GetStep()->GetPreStepPoint()->GetPhysicalVolume()->GetName() << G4endl;
+    G4cout << "Post: " << aTrack->GetStep()->GetPostStepPoint()->GetPhysicalVolume()->GetName() << G4endl;
+    //if(aTrack->GetStep()->GetPreStepPoint()->GetPhysicalVolume()->GetName() == "Ge_phys")
+  }
+
   G4ClassificationOfNewTrack classification = fUrgent;
 
   if(aTrack->GetParentID() == 0)  // Primary particle

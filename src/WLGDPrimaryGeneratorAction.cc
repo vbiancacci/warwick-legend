168c3461c165b59644e3f86384365cec3c64aed5
Moritz Neuberger <moritz.neuberger@tum.de>
2021-03-22 00:40:55 +0100

    increased wall size and added larger MUSUN file

--- src/WLGDPrimaryGeneratorAction.cc
+++ src/WLGDPrimaryGeneratorAction.cc
@@ -127,6 +127,7 @@ void WLGDPrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
 
     fInputFile >> nEvent >> particleID >> energy >> x >> y >> z >> theta >> phi;
 
+    G4cout << x << " " << y << " " << z << G4endl;
     if (fInputFile.eof())
     {
       fInputFile.close();

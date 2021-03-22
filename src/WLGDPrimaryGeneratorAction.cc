914cef11619f90e40df98adc842b897f03a144f9
Moritz Neuberger <moritz.neuberger@tum.de>
2021-03-22 20:00:29 +0100

    added energy output of neutrons, still not resolved the musun input

--- src/WLGDPrimaryGeneratorAction.cc
+++ src/WLGDPrimaryGeneratorAction.cc
@@ -219,7 +219,7 @@ void WLGDPrimaryGeneratorAction::DefineCommands()
     .SetGuidance("Set MUSUN file name")
     .SetParameterName("filename", false)
     .SetDefaultValue("./musun_gs_100M.dat")
-    //.SetStates(G4State_Idle)
+    .SetStates(G4State_Idle)
     .SetToBeBroadcasted(false);
 
   // generator command

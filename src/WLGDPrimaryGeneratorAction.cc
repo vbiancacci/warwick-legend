32c9158e8288cc66a54ce5eb2660a1fe1c540665
Moritz Neuberger <moritz.neuberger@tum.de>
2021-03-22 20:16:09 +0100

    added energy output of neutrons, still not resolved the musun input

--- src/WLGDPrimaryGeneratorAction.cc
+++ src/WLGDPrimaryGeneratorAction.cc
@@ -70,6 +70,18 @@ void WLGDPrimaryGeneratorAction::ChangeFileName(G4String newFile)
     G4cout << "opening file: " << newFile << G4endl;
     fFileName = newFile;
     OpenFile(); //open the new one
+
+    G4int nEvent=0;
+    G4double time=0.0;
+    G4double energy = 0.0*MeV;
+    G4double px,py,pz;
+    G4double theta,phi;
+    G4double x = 0, y = 0, z = 0;
+    G4int particleID = 0;
+
+    WLGDPrimaryGeneratorAction::fInputFile >> nEvent >> particleID >> energy >> x >> y >> z >> theta >> phi;
+
+    G4cout << fInputFile.is_open() << " " << x << " " << y << " " << z << G4endl;
   }
 }
 

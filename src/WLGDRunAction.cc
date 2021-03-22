24ae66cf9ee874f9a21d554a8077bd7e3cd5cf19
Moritz Neuberger <moritz.neuberger@tum.de>
2021-03-22 17:00:03 +0100

    added energy output of neutrons, still not resolved the musun input

--- src/WLGDRunAction.cc
+++ src/WLGDRunAction.cc
@@ -29,9 +29,9 @@ WLGDRunAction::WLGDRunAction(WLGDEventAction* eventAction, G4String name, WLGDPr
   // Create analysis manager
   auto analysisManager = G4AnalysisManager::Instance();
 
-
-
-  primAction->ChangeFileName("./musun_gs_100M.dat");
+  string fileName_Musun = "./musun_gs_100M.dat";
+  G4cout << "Musun filename: " << fileName_Musun << G4endl;
+  primAction->ChangeFileName(fileName_Musun);
 
   // Create directories
   analysisManager->SetVerboseLevel(1);

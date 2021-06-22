b259c75f9fe4d93a200ee85bed096a5256e605d9
Neuberger <moritz.neuberger@tum.de>
2021-06-22 12:00:24 +0200

    test

--- src/WLGDPrimaryGeneratorAction.cc
+++ src/WLGDPrimaryGeneratorAction.cc
@@ -231,11 +231,11 @@ void WLGDPrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
 
         G4ParticleTable* theParticleTable = G4ParticleTable::GetParticleTable();
 
-        G4cout << theParticleTable->GetIonTable(32,77,159.71)->GetLifeTime() << G4endl;
-        fParticleGun->SetParticleDefinition(theParticleTable->GetIonTable()->FindIon(32,77,159.7));
+        G4cout << theParticleTable->GetIonTable(32,77,159.71,0)->GetLifeTime() << G4endl;
+        fParticleGun->SetParticleDefinition(theParticleTable->GetIonTable()->FindIon(32,77,159.7,0));
 
 
-        G4double theMass = theParticleTable->GetIonTable()->FindIon(32,77,159.7)->GetPDGMass();
+        G4double theMass = theParticleTable->GetIonTable()->FindIon(32,77,159.7,0)->GetPDGMass();
         G4double totMomentum = std::sqrt(energy*energy+2*theMass*energy);
         G4double pz = -1*std::cos(theta);
         G4double px = std::sin(theta)*cos(phi);

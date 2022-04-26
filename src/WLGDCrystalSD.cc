ecdc361ad085cef3833d1fcd9c3deac1ee47843d
Neuberger <moritz.neuberger@tum.de>
2022-04-26 16:36:13 +0200

    found a place where the copy number saved was not adjusted yet

--- src/WLGDCrystalSD.cc
+++ src/WLGDCrystalSD.cc
@@ -75,9 +75,13 @@ G4bool WLGDCrystalSD::ProcessHits(G4Step* aStep, G4TouchableHistory*)
   if(fGeometryName == "hallA")
     whichReentranceTube = 0;
   newHit->SetWhichReentranceTube(whichReentranceTube);
-  newHit->SetWhichDetector(
-    aStep->GetPostStepPoint()->GetTouchable()->GetVolume(1)->GetCopyNo() +
-    whichReentranceTube * 96);
+  if(fGeometryName == "baseline_large_reentrance_tube")
+    newHit->SetWhichDetector(
+      aStep->GetPostStepPoint()->GetTouchable()->GetVolume(1)->GetCopyNo());
+  else
+    newHit->SetWhichDetector(
+      aStep->GetPostStepPoint()->GetTouchable()->GetVolume(1)->GetCopyNo() +
+      whichReentranceTube * 96);
   fHitsCollection->insert(newHit);
 
   return true;
